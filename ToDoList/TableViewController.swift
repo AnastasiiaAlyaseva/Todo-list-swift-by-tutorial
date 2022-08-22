7c4ebc4798846b9dd2ee7a2854cf220b505a1ad1
Anastasiia Alyaseva <180915an@gmail.com>
2022-08-22 13:17:17 +0200

    lesson 20

--- ToDoList/TableViewController.swift
+++ ToDoList/TableViewController.swift
@@ -11,6 +11,9 @@ class TableViewController: UITableViewController {
     
     @IBAction func pushEditAction(_ sender: Any) {
         tableView.setEditing(!tableView.isEditing, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            self.tableView.reloadData()
+        }
     }
     
     @IBAction func pushAddAction(_ sender: Any) {
@@ -37,6 +40,8 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -66,6 +71,13 @@ class TableViewController: UITableViewController {
             cell.imageView?.image = UIImage(named: "check")
         } else {
             cell.imageView?.image = UIImage(named: "uncheck")
+        }
+        if tableView.isEditing {
+            cell.textLabel?.alpha = 0.4
+            cell.imageView?.alpha = 0.4
+        } else {
+            cell.textLabel?.alpha = 1
+            cell.imageView?.alpha = 1
         }
 
         return cell
@@ -115,8 +127,17 @@ class TableViewController: UITableViewController {
         
 
     }
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if tableView.isEditing {
+        return .none
+        } else {
+            return .delete
+        }
+    }
     
-
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
     /*
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
